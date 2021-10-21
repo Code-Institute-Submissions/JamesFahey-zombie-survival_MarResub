ae340eb88f24b55c82c80ef251a7daa3672d0d29
JamesFahey <james.fahey@hotmail.co.uk>
2021-10-21 16:33:54 +0000

    add function for first choice - leave

--- run.py
+++ run.py
@@ -51,11 +51,47 @@ def start():
         elif answer == "1":
             # allow player to exit
             print("Too scared to see if you would of made it?")
-            print("Doubt you would of got far")
+            print("Doubt you would of got far anyway")
             # exit()
     
 def leave():
+    print("You pack up all remaining food and supplies, wait for it to be clear outside before making your move.")
+    time.sleep(2)
+    print("Do you take the car which has a full tank of petrol ready from the camping trip")
+    time.sleep(2)
+    print("or go out the back way which leads to a now unused railway line.")
+    time.sleep(2)
+    print("Car or Back")
+
+    answer = input(">").lower().strip()
+    if answer == "car":
+        # take player to cross_roads
+        car()
+    elif answer == "back":
+        # take player to play_again()
+        back()
+    else:
+        # else return player to start()
+        print("\n Incorrect Answer")
+        print("\n To continue playing type either car or back. To quit the game press 1")
+        answer = input(">").lower().strip()
+        if answer == "car":
+        # take player to cross_roads
+            car()
+        elif answer == "back":
+        # take player to play_again()
+            back()
+        elif answer == "1":
+            # allow player to exit
+            print("Too scared to see if you would of made it?")
+            print("Doubt you would of got far anyway")
+            # exit()
+
+def car():
     print("hello")
+
+def back():
+    print("hello") 
 
 def stay():
     print("hello")
